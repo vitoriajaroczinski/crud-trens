cd0eb27793e6105917da851fbd72e910b5b96f70
vitoriajaroczinski <vitoria_jaroczinski@estudante.sesisenai.org.br>
2026-08-18 08:35:31 -0300

    Início do CRUD de trens

---
+++ conexao.php
@@ -0,0 +1,16 @@
+<?php
+
+$servidor = 'localhost';
+$usuario = 'root';
+$senha = 'root';
+$banco = 'frota_ferroviaria';
+
+$conexao = new mysqli($servidor, $usuario, $senha, $banco);
+
+if($conexao->connect_error){
+    die('Falha na conexão: '. $conexao->connect_error);
+}
+
+$conexao->set_charset('utf8mb4');
+
+?>
